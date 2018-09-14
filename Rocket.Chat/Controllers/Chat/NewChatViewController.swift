b975e663c08b1d5cd7cc02bbd8047f3535f8c217
filipealva <ofilipealvarenga@gmail.com>
2018-09-14 10:32:38 -0300

    Integrate RocketChatViewController and create a temporary NewChatViewController to test the integration

---
+++ Rocket.Chat/Controllers/Chat/NewChatViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewChatViewController.swift
+//  Rocket.Chat
+//
+//  Created by Filipe Alvarenga on 14/09/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+import RocketChatViewController
+
+class NewChatViewController: RocketChatViewController {
+
+}
